2f0daac01c9accac891040075afec691bd5ca49d
someshbvrm <somesh910@gmail.com>
2025-09-09 17:17:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,21 +28,11 @@ resource "random_id" "id" {
   byte_length = 4
 }
 
-# Create an AWS key pair using the public key from Jenkins
-resource "aws_key_pair" "deployer" {
-  key_name   = "keypair_stockholms"
-  public_key = var.ssh_public_key
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes = [public_key]
-  }
-}
-
 # Launch EC2 instance in default VPC & default security group
 resource "aws_instance" "app" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = "keypair_stockholms"
   associate_public_ip_address = true   # ensure public IP in default VPC
 
   # By default, AWS assigns the default security group of the default VPC
