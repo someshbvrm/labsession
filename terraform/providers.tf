8642e86b505425bb2c0db071d95ed839dc358b4a
someshbvrm <somesh910@gmail.com>
2025-09-09 16:53:29 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
 
-provider "aws" {
-  region = var.aws_region
-}
